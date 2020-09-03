ba8acf24def67479014978d3c948ec554da29a48
Benjamin Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-03 01:14:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,7 +15,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index)
+  if position_taken?(board, index) || board[index] != (0 - 8)
     return false
   else between?(board, index)
     return true
