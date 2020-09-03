cc89c00c37e0eebde5f1b42a5dca3d2449d6a97e
Benjamin Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-03 01:05:14 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -17,7 +17,7 @@ end
 def valid_move?(board, index)
   if position_taken?(board, index)
     return false
-  else between?(board, index)
+  else between?(board, index) > 8
     return true
   end
 end
